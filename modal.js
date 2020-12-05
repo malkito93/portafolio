809cbc9074d68b856136c54bdb643b77fa5b881e
Marcos <malkito93@hotmail.com>
2020-12-05 00:56:43 -0300

    responsive

--- modal.js
+++ modal.js
@@ -5,12 +5,16 @@ modal.forEach (galery_img =>{
         // alert('click')
         galery_img.classList.toggle('active')
 })
+});
+
+
+// boton menu responsive
+const burger_menu = document.getElementById('burger_menu')
+const ul_menu = document.getElementsByClassName('ul_menu')
+
+burger_menu.addEventListener('click',function(){
+        ul_menu[0].classList.toggle('show')
 })
 
 
 // boton de cerrar modal
-
-
-
-
-
